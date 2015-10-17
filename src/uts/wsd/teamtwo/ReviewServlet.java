5968b1569f885005c70ac61bbfb7a3bb4f6a514a
Deinyon Davies <deinyond@gmail.com>
2015-10-17 22:06:58 +1100

    Review Validation

--- src/uts/wsd/teamtwo/ReviewServlet.java
+++ src/uts/wsd/teamtwo/ReviewServlet.java
@@ -2,6 +2,7 @@ package uts.wsd.teamtwo;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
 import javax.xml.bind.JAXBException;
+import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
 
@@ -90,20 +92,37 @@ public class ReviewServlet extends HttpServlet
 	
 	private void postReview(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
+		// Double-check that the author is logged in
 		Author author = (Author) request.getSession().getAttribute("author");
 		if(author == null)
 			return;
 		
+		// Obtain POST parameters
 		String
+			hotelIdString = request.getParameter("hotelId"),
+			ratingString = request.getParameter("rating"),
 			reviewTitle = request.getParameter("title"),
 			reviewMessage = request.getParameter("message");
 		
-		int
-			hotelId = Integer.parseInt(request.getParameter("hotelId")),
-			reviewRating = Integer.parseInt(request.getParameter("rating"));
+		// Obtain the hotel ID now
+		int hotelId = Integer.parseInt(hotelIdString);
 		
+		// Verify that the data exists and is valid
+		ComposeReviewErrorFields errors = 
+				verifyPostReviewData(reviewTitle, ratingString, reviewMessage);
+		if(errors != ComposeReviewErrorFields.NONE)
+		{
+			returnToPageWithError(hotelId, errors, request, response);
+			return;
+		}
+		
+		// Parse integer variables
+		int reviewRating = Integer.parseInt(ratingString);
+		
+		// Obtain the current date
 		Date postDate = new Date();
 		
+		// Create and post the new review
 		Review review = new Review(hotelId, author.getId(), reviewTitle, reviewRating, postDate, reviewMessage);
 		try {
 			getReviewsApp(request.getServletContext()).postReview(review);
@@ -148,6 +167,65 @@ public class ReviewServlet extends HttpServlet
 		redirectToHotelPage(hotelId, request, response);
 	}
 	
+	private ComposeReviewErrorFields verifyPostReviewData(String title, String ratingString, String message)
+	{
+		ComposeReviewErrorFields error;
+		
+		error = validateTitleParam(title);
+		if(error != ComposeReviewErrorFields.NONE)
+			return error;
+		
+		error = validateRatingParam(ratingString);
+		if(error != ComposeReviewErrorFields.NONE)
+			return error;
+		
+		error = validateMessageParam(message);
+		if(error != ComposeReviewErrorFields.NONE)
+			return error;
+		
+		return ComposeReviewErrorFields.NONE;
+	}
+	
+	private ComposeReviewErrorFields validateTitleParam (String title)
+	{
+		// Check for empty parameters
+		if(title == null || title.isEmpty())
+			return ComposeReviewErrorFields.TITLE_MISSING;
+		
+		// Check title format
+		Pattern titleRegex = Pattern.compile("([A-Z]\\w*\\s?)+");
+		Boolean patternMatches = titleRegex.matcher(title).matches();
+		if(!patternMatches)
+			return ComposeReviewErrorFields.TITLE_FORMAT;
+		
+		return ComposeReviewErrorFields.NONE;
+	}
+	
+	private ComposeReviewErrorFields validateRatingParam (String rating)
+	{
+		// Check for empty parameters
+		if(rating == null || rating.isEmpty())
+			return ComposeReviewErrorFields.RATING_MISSING;
+		
+		// Check rating format
+		int ratingInteger;
+		try { ratingInteger = Integer.parseInt(rating); }
+		catch (Exception e) { return ComposeReviewErrorFields.RATING_FORMAT; }
+		if(ratingInteger < 1 || ratingInteger > 10)
+			return ComposeReviewErrorFields.RATING_FORMAT;
+		
+		return ComposeReviewErrorFields.NONE;
+	}
+	
+	private ComposeReviewErrorFields validateMessageParam (String message)
+	{
+		// Check for empty message
+		if(message == null || message.isEmpty())
+			return ComposeReviewErrorFields.MESSAGE_MISSING;
+		
+		return ComposeReviewErrorFields.NONE;
+	}
+	
 	/**
 	 * Redirect the client to the hotel detail page with the specified ID
 	 * @param hotelId The ID of the hotel page to display
@@ -156,6 +234,17 @@ public class ReviewServlet extends HttpServlet
 	{
 		String redirectUrl = "hotel.jsp?id=" + hotelId;
 		RequestDispatcher dispatcher = request.getRequestDispatcher(redirectUrl);
+
+		dispatcher.forward(request, response);
+	}
+	
+	private void returnToPageWithError(int hotelId, ComposeReviewErrorFields errorFields, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String redirectUrl = "hotel.jsp?id=" + hotelId;
+		RequestDispatcher dispatcher = request.getRequestDispatcher(redirectUrl);
+		
+		request.setAttribute("composeReviewError", errorFields);
+
 		dispatcher.forward(request, response);
 	}
 }
